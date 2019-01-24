c32d68f51e484524b581ff8aecc2eb753a7a5313
CM Student <cmstudent@UHWO-ACM-18.local>
2019-01-24 09:53:03 -1000

    Initial Commit

---
+++ HackwichTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackwichTwo
+//
+//  Created by CM Student on 1/24/19.
+//  Copyright © 2019 Naohiro Kiryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
